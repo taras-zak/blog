d006c03ae44122ff27df44be04a1396da151e0bc
TarasZak <taras.zakan@gmail.com>
2015-07-19 15:24:56 +0300

    add likes

--- article/urls.py
+++ article/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include, url,patterns
 
 urlpatterns = [
     url(r'^all', 'article.views.articles'),
-    url(r'^get/(?P<article_id>\d+)/$', 'article.views.article'),
+    url(r'^get/(?P<article_id>\d+)', 'article.views.article'),
+    url(r'^addLike/(?P<article_id>\d+)', 'article.views.addLike'),
     url(r'^', 'article.views.articles'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from article.models import Article, Comments
 from django.template.loader import get_template
 from django.template import Context
+from django.core.exceptions import ObjectDoesNotExist
+from django.http.response import Http404
 
 
 # Create your views here
@@ -14,3 +16,13 @@ def article(request, article_id = 1):
                                 {'article': Article.objects.get(id = article_id), 
                                  'comments': Comments.objects.filter(comments_article_id = article_id)}
                              )
+
+def addLike(request, article_id):
+    try:
+        article = Article.objects.get(pk = article_id)
+        article.article_likes += 1
+        article.save()
+    except ObjectDoesNotExist:
+        raise Http404
+
+    return redirect('/')
