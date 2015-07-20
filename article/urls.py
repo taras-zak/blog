b6874745b2082ec2e7dbdea9262239fb029220e4
TarasZak <taras.zakan@gmail.com>
2015-07-20 14:02:11 +0300

    add comments

--- article/urls.py
+++ article/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^all', 'article.views.articles'),
     url(r'^get/(?P<article_id>\d+)', 'article.views.article'),
     url(r'^addLike/(?P<article_id>\d+)', 'article.views.addLike'),
+    url(r'^addComment/(?P<article_id>\d+)', 'article.views.addComment'),
     url(r'^', 'article.views.articles'),
 ]
--- article/views.py
+++ article/views.py
@@ -4,6 +4,8 @@ from django.template.loader import get_template
 from django.template import Context
 from django.core.exceptions import ObjectDoesNotExist
 from django.http.response import Http404
+from forms import CommentForm
+from django.template.context_processors import csrf
 
 
 # Create your views here
@@ -11,11 +13,13 @@ def articles(request):
     return render_to_response('articles.html', {'articles': Article.objects.all()})
 
 def article(request, article_id = 1):
-    return render_to_response( 
-                                'article.html', 
-                                {'article': Article.objects.get(id = article_id), 
-                                 'comments': Comments.objects.filter(comments_article_id = article_id)}
-                             )
+    comment_form = CommentForm
+    args = {'article': Article.objects.get(id = article_id), 
+            'comments': Comments.objects.filter(comments_article_id = article_id),
+            'form': comment_form,
+            }
+    args.update(csrf(request))
+    return render_to_response('article.html', args)
 
 def addLike(request, article_id):
     try:
@@ -26,3 +30,14 @@ def addLike(request, article_id):
         raise Http404
 
     return redirect('/')
+
+def addComment(request, article_id):
+    if request.POST:
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit = False)
+            comment.comments_article = Article.objects.get(id = article_id)
+            form.save()
+    return redirect('/get/%s' % article_id)
+
+
