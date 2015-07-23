e565ccf3a88a56f1db0d7324cec2d72f02a419e8
TarasZak <taras.zakan@gmail.com>
2015-07-23 13:39:16 +0300

    add registration of user

--- loginsys/urls.py
+++ loginsys/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import include, url,patterns
 urlpatterns = [
     url(r'^login/', 'loginsys.views.login'),
     url(r'^logout/', 'loginsys.views.logout'),
+    url(r'^register/', 'loginsys.views.register'),
 ]
--- loginsys/views.py
+++ loginsys/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response, redirect
 from django.template.context_processors import csrf
 from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def login(request):
@@ -22,3 +23,21 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('/')
+
+def register(request):
+    args = {}
+    args.update(csrf(request))
+    args['form'] = UserCreationForm()
+    if request.POST:
+        newuser_form = UserCreationForm(request.POST)
+        if newuser_form.is_valid():
+            newuser_form.save()
+            newuser = auth.authenticate (
+                    username=newuser_form.cleaned_data['username'], 
+                    password=newuser_form.cleaned_data['password2']
+                    )
+            auth.login(request, newuser)
+            return redirect('/')
+        else:
+            args['form'] = newuser_form
+    return render_to_response('register.html', args)
